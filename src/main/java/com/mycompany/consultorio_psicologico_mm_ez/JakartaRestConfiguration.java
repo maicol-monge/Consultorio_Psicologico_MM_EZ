350f451c2477008fd2c8be7d25ba24e3c5ebb227
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-10-05 21:23:45 -0600

    Hello World

---
+++ src/main/java/com/mycompany/consultorio_psicologico_mm_ez/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.consultorio_psicologico_mm_ez;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
